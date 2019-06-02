08797ae196ce759874f048415e1efaf36c080940
Joe Daddario <joe@shrthnd.org>
2019-06-02 10:47:14 -0400

    Add contributors, test, and requirements fields to plugin header

--- advanced-bootstrap-blocks.php
+++ advanced-bootstrap-blocks.php
@@ -3,9 +3,13 @@
  * Plugin Name: Advanced Bootstrap Blocks
  * Plugin URI: https://www.wordpress.org/plugins/advanced-bootstrap-blocks/
  * Description: Flexible Bootstrap 4 blocks for building custom page layouts in the Wordpress editor (aka Gutenberg.) 
+ * Tags: bootstrap, gutenberg, editor, blocks
+ * Contributors: helpful.dev
  * Author: helpful.dev
  * Author URI: https://helpful.dev/
- * Version: 1.0.0-beta
+ * Version: 1.0.1-beta
+ * Tested up to: 5.2.1
+ * Requires at least: 5.0.0
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
