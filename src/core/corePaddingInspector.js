bc41f7377960fbe7dba72aeb6babc544c2221bbe
Joe Daddario <joe@shrthnd.org>
2020-03-24 15:44:01 -0400

    Add regex to replace class and return padding value

--- src/core/corePaddingInspector.js
+++ src/core/corePaddingInspector.js
@@ -1,131 +1,138 @@
 const { __ } = wp.i18n;
-const { Fragment } = wp.element;
+const { Fragment, useEffect, useRef } = wp.element;
 const { InspectorControls } = wp.blockEditor;
-const { PanelBody, PanelRow } = wp.components;
+const { PanelBody, PanelRow, RangeControl } = wp.components;
 const { createHigherOrderComponent, withState } = wp.compose;
 
 export const CustomPaddingInspector = createHigherOrderComponent( ( BlockEdit ) => {
 	return ( props ) => {
     if (props.name.includes("advanced-bootstrap-blocks") || props.name.includes("core")) {
-
-      const isAttrDefined = (attribute) => {
-        return typeof attribute !== "undefined" && attribute.length ? attribute : '';
-      }
-
-      const isPaddingSet = (list, attribute) => {
-        // todo: check if padding is set and prepopulate control
-        
-        return isAttrDefined(attribute); 
-      }
-
-      const filterPaddingClassByPrefix = (list, prefix) => {
-          return list
-            .split(" ")
-            .filter(name => !name.includes(prefix));
-      }
-
-      const onChangePadding = (target) => {
-        const paddingType = target.getAttribute("data-padding-type");
-        const paddingBreakpoint = target.getAttribute("data-padding-breakpoint");
-        const paddingValue = target.value; 
-
-        const newClassNamePaddingPrefix = `p${paddingType}-${paddingBreakpoint}-`.replace('a','').replace('-xs', '');
-        const newClassNamePaddingClass = paddingValue != '' ? `${newClassNamePaddingPrefix}${paddingValue}` : ''; 
-        const classNameArray = filterPaddingClassByPrefix(classNameList, newClassNamePaddingPrefix);
-
-        const classNameListUpdated = classNameArray
-          .concat(newClassNamePaddingClass)
-          .join(' ')
-          .trim()
-          .replace(/\s\s+/, ' ');
-
-        props.setAttributes( { 
-          className: classNameListUpdated
-        }); 
-        
-        props.setAttributes({ 
-          [target.id]: paddingValue 
-        });
-        
-        console.log(target.id, eval(`props.attributes.${[target.id]}`))
-      }
       
-      const onClickPaddingReset = (classList, classId) => {
-        const classPaddingStateObject = eval(`props.attributes.${classId}`);
-        props.setAttributes({
-          className: filterPaddingClassByPrefix(classList, classPaddingStateObject)
-            .join(' ')
-            .trim()
-            .replace(/\s\s+/, ' ')
-        });
+      // const properties = ['a','x','y','t','r','b','l'];
+      // const breakpoints = ['xs','sm','md','lg','xl'];
 
-        props.setAttributes({ 
-          [classId]: '' 
-        });
-      }
-
-      const classNameList = isAttrDefined(props.attributes.className); 
-      const variants = ['a','x','y','t','r','b','l'];
+      const properties = ['a','x','y','t','r','b','l'];
       const breakpoints = ['xs','sm','md','lg','xl'];
 
-      const PaddingControls = ({ breakpoint, variant }) => {
-       
-            const controlId = `p${variant}${breakpoint}`;
-            const attrName = `props.attributes.${controlId}`; 
-            const value = isPaddingSet(classNameList, eval(attrName));
+      const variants = [];
 
-            return <PanelRow key={ controlId } className="flex-wrap align-items-center">
-              <label 
-                className="d-block w-100 m-0 small text-left"
-                htmlFor={ controlId }>Padding ({controlId})</label>
-              <input 
-                type="range"
-                id={ controlId }
-                data-padding-type={ variant }
-                data-padding-breakpoint={ breakpoint }
-                value={ value }
-                onChange={
-                  e => onChangePadding(e.target)
+      const strToRegex = (property, breakpoint) => {
+        // const regex = /(p{1}[trblxy]?[-]?\b(sm|md|lg|xl)?[-]{1}(auto\b|[0-5]\b){1}?)/;
+        const paddingProperty = property.replace('a', '');
+        const paddingBreakpoint = breakpoint.replace('xs', ''); 
+        const regexString = `p{1}(property)[-](breakpoint)[-]?([0-5]\\b)`
+          .replace('property', `${paddingProperty}`)
+          .replace('breakpoint', `${paddingBreakpoint}`);
+
+        return new RegExp(regexString); 
+      }
+
+      const removePaddingClass = (property, breakpoint) => {
+        if (typeof props.attributes.className !== "undefined") {
+        const regex = strToRegex(property, breakpoint); 
+
+          return props.attributes.className
+            .split(" ")
+            .filter(name => !name.match(regex)); 
+        }
+      }
+
+      const returnPaddingValue = (property, breakpoint) => {
+        if (typeof props.attributes.className !== "undefined") {
+          const regex = strToRegex(property, breakpoint); 
+          const results = props.attributes.className.length && props.attributes.className.match(regex) ? Number(props.attributes.className.match(regex)[3]) : -1; 
+          if (results > -1) {
+            return results;
+          }
+        }
+      }
+      
+      breakpoints.map( breakpoint => {
+        properties.map( property => { 
+          const paddingValue = returnPaddingValue(property, breakpoint); 
+          variants.push({
+            ref: useRef(`p${property}-${breakpoint}`),
+            property: property,
+            breakpoint: breakpoint,
+            defaultValue: typeof paddingValue !== "undefined" ? paddingValue : '',
+          });
+        });
+      });
+      
+      const PaddingControl = withState( {
+        padding: '',
+      } )( 
+        ( { ref, defaultValue, padding, setState, property, breakpoint } ) => {
+
+          // useEffect(() => {
+          //   if (typeof padding !== "undefined" && padding.toString().length && padding > -1) {
+          //     const newClassNamePaddingPrefix = `p${property}-${breakpoint}-`.replace('a','').replace('-xs', '');
+          //     const newClassNamePaddingClass = padding >= 0 ? `${newClassNamePaddingPrefix}${padding}` : ''; 
+          //     const classNameArray = removePaddingClass(property, breakpoint) || [];
+                
+          //     const classNameListUpdated = typeof classNameArray !== "undefined" && classNameArray
+          //       .concat(newClassNamePaddingClass)
+          //       .join(' ')
+          //       .trim()
+          //       .replace(/\s\s+/, ' ');
+                            
+          //     // return () => {
+          //       props.setAttributes( { 
+          //         className: classNameListUpdated
+          //       });  
+          //     // }
+          //   }
+          // }, [padding]);
+
+          return (
+            <PanelRow>
+              <RangeControl
+                ref={ ref }
+                key={ ref }
+                label={ `.p${property}-${breakpoint}` }
+                value={ typeof padding !== "undefined" && padding > -1 && padding || defaultValue}
+                defaultValue={ defaultValue }
+                onChange={ 
+                  padding => {
+                    // props.setAttributes('')
+                    let classNameListUpdated; 
+                    if (typeof padding !== "undefined" && padding.toString().length && padding > -1) {
+                      const newClassNamePaddingPrefix = `p${property}-${breakpoint}-`.replace('a','').replace('-xs', '');
+                      const newClassNamePaddingClass = padding >= 0 ? `${newClassNamePaddingPrefix}${padding}` : ''; 
+                      const classNameArray = removePaddingClass(property, breakpoint) || [];
+                        
+                      classNameListUpdated = typeof classNameArray !== "undefined" && classNameArray
+                        .concat(newClassNamePaddingClass)
+                        .join(' ')
+                        .trim()
+                        .replace(/\s\s+/, ' ');
+                                    
+
+                    } else {
+                      const classNameArray = removePaddingClass(property, breakpoint) || [];
+                      classNameListUpdated = typeof classNameArray !== "undefined" && classNameArray
+                        .join(' ')
+                        .trim()
+                        .replace(/\s\s+/, ' ');
+                    }
+                    
+                    props.setAttributes( { 
+                      className: classNameListUpdated
+                    });  
+                    
+                    setState( { padding } );
+                    
+                  }
                 }
-                min="0"
-                max="5"
-                step="1"
-                style={{
-                  width: "60%"
-                }}
+                min={ 0 }
+                max={ 5 }
+                allowReset
               />
-
-              <label 
-                className="sr-only"
-                htmlFor={ `${controlId}_value` }>Padding Value ({controlId})</label>
-              <input 
-                id={ `${controlId}_value` }
-                data-padding-type={ variant }
-                data-padding-breakpoint={ breakpoint }
-                value={ value }
-                type="number" 
-                min="0" 
-                max="5" 
-                style={{ width: '15%' }}
-                disabled
-              />
-
-              <button 
-                className="btn btn-sm btn-outline-dark"
-                type="button"
-                onClick={ () => onClickPaddingReset(classNameList, controlId) }
-                style={{
-                  width: "10%",
-                  minWidth: "40px"
-                }}
-              >
-                <i className="fa fa-trash" aria-hidden="true"></i>
-                <span className="sr-only">Remove Padding Class ({controlId})</span>
-              </button>
             </PanelRow>
-
-      };
-
+          );
+        }
+      );
+      
       return (
         <Fragment>
           <BlockEdit { ...props } />
@@ -135,13 +142,18 @@ export const CustomPaddingInspector = createHigherOrderComponent( ( BlockEdit ) 
               initialOpen={false}
             >
             {
-              breakpoints.map( breakpoint => {
-                return variants.map( variant => { 
-                  return <PaddingControls key={`${breakpoint}_${variant}`} breakpoint={breakpoint} variant={variant}/>
-                });
+              variants.map(variant => {
+                return (
+                  <PaddingControl 
+                    key={ variant.ref } 
+                    ref={ variant.ref } 
+                    property={ variant.property } 
+                    breakpoint={ variant.breakpoint }
+                    defaultValue={ variant.defaultValue }
+                  />
+                );
               })
             }
-              
             </PanelBody>
           </InspectorControls>
         </Fragment>
